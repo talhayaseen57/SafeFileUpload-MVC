87dc6e7cf852d92a1a51fa689ed51709c3110168
Talha Yaseen Zafar <talhayaseen5725@gmail.com>
2025-05-27 15:54:50 +0200

    Add project files.

---
+++ SafeFileUpload MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SafeFileUpload_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
